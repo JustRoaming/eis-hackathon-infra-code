cfead7687035c0dca7001d7eb2bc74b6349b77f5
Wayne Crow <wayne.crow@edfenergy.com>
2024-06-06 16:07:58 +0100

    updated tf backend config mk2

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,5 +1,9 @@
-resource_group_name  = "terraform"
-storage_account_name = "eisworkshop"
-container_name       = "state"
-subscription_id      = "652a7ae0-ba5c-44a3-aa80-69e61171f84c"
-key                  = "dev-hackathon-team-a.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "eisworkshop"
+    container_name       = "tfstate"
+    subscription_id      = "652a7ae0-ba5c-44a3-aa80-69e61171f84c"
+    key                  = "dev-hackathon-team-a.tfstate"
+  }
+}
